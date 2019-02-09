5aaadecc69c1fc957644e88ae6e894b71fba6f84
willemserruys <willem.serruys@vinci-energies.net>
2019-02-09 23:54:41 +0100

    added material module

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -4,12 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoreModule } from 'src/app/Core/core.module';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
     CoreModule.forRoot()
   ],
   providers: [],
